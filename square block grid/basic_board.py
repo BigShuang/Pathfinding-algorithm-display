59b589059c857a6767045b6747fa6f70cfa95bcf
BigShuang <3508645145@qq.com>
2021-04-29 09:50:00 +0800

    basic complete: save and load

--- square block grid/basic_board.py
+++ square block grid/basic_board.py
@@ -6,8 +6,16 @@ FPS=60 # 游戏帧率
 C, R = 20, 20  # 11列， 20行
 CELL_SIZE = 40  # 格子尺寸
 
-WIN_WIDTH = CELL_SIZE * C  # 窗口宽度
-WIN_HEIGHT = CELL_SIZE * R  # 窗口高度
+PADDING = {
+    "left": 100,
+    "right": 100,
+    "top": 50,
+    "bottom": 50,
+}
+
+
+WIN_WIDTH = CELL_SIZE * C + PADDING["left"] + PADDING["right"]  # 窗口宽度
+WIN_HEIGHT = CELL_SIZE * R + PADDING["top"] + PADDING["bottom"]  # 窗口高度
 
 
 pygame.init() # pygame初始化，必须有，且必须在开头
@@ -16,11 +24,20 @@ clock = pygame.time.Clock() # 用于控制循环刷新频率的对象
 win = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
 pygame.display.set_caption('Basic Board By Big Shuang')
 
+center_surface = pygame.Surface((CELL_SIZE * C, CELL_SIZE * R))
+
+# 大中小三种字体，48,36,24
+FONTS = [
+    pygame.font.Font(pygame.font.get_default_font(), font_size) for font_size in [48, 36, 24]
+]
+
 
 COLORS = {
-    "bg": (200, 200, 200),
+    "bg": (150,150,150),
+    "board": (200, 200, 200),
     "wall": (50, 50, 50),
-    "line": (175, 175, 175)
+    "line": (175, 175, 175),
+    "menu": (65, 105, 225)
 }
 
 win.fill(COLORS["bg"])
@@ -38,6 +55,7 @@ class Block(pygame.sprite.Sprite):
         x, y = c * size, r * size
         self.rect.move_ip(x, y)
 
+
 class Board(pygame.sprite.Group):
     def __init__(self, master, c, r, size):
         super().__init__()
@@ -52,6 +70,8 @@ class Board(pygame.sprite.Group):
 
     def draw_wall_by_xy(self, x, y):
         ci, ri = x // self.size, y // self.size
+        if ci < 0 or ri < 0 or ci >= self.c or ri >= self.r:
+            return
         cell = self.board[ri][ci]
 
         if cell is None:
@@ -61,11 +81,55 @@ class Board(pygame.sprite.Group):
 
     def remove_wall_by_xy(self, x, y):
         ci, ri = x // self.size, y // self.size
+        if ci < 0 or ri < 0 or ci >= self.c or ri >= self.r:
+            return
+
         cell = self.board[ri][ci]
 
         if cell is not None:
             self.remove(cell)
             self.board[ri][ci] = None
+
+    def to_txt(self):
+        ss = []
+        for row in self.board:
+            row_str = []
+            for cell in row:
+                if cell is None:
+                    row_str.append("0")
+                else:
+                    row_str.append("1")
+            ss.append(" ".join(row_str))
+
+        return "\n".join(ss)
+
+    def load_txt(self, filename):
+        with open(filename, 'r') as f:
+            fl = f.read()
+        lines = fl.split("\n")
+        if len(lines) != R:
+            print("txt r is not valid")
+            return
+
+        for line in lines:
+            line = line.replace(" ", "")
+            if len(line) != C:
+                print("txt c is not valid")
+                return
+
+        self.empty()
+
+        for ri, line in enumerate(lines):
+            line = line.replace(" ", "")
+            for ci, cell in enumerate(line):
+                if cell == "1":
+                    cell = Block(ci, ri, self.size, COLORS["wall"])
+                    self.add(cell)
+                    self.board[ri][ci] = cell
+                elif cell == "0":
+                    self.board[ri][ci] = None
+                else:
+                    self.board[ri][ci] = None
 
 
 def draw_lines(c, r, win):
@@ -78,7 +142,27 @@ def draw_lines(c, r, win):
         pygame.draw.line(win, COLORS["line"], (0, ry), (c * CELL_SIZE, ry))
 
 
+def draw_menu_1(left, top, font):
+    save = font.render("Ctrl+S to Save. Ctrl+D to load", True, COLORS["menu"])
+    text_rect = save.get_rect(midleft=(left, top / 2))
+    win.blit(save, text_rect)
+
+
+def save_to_txt(board, filename):
+    try:
+        with open(filename, 'w') as f:
+            f.write(board.to_txt())
+        print("Saved to %s." % filename)
+    except Exception as e:
+        print("save failed:", e)
+
+
+
 board = Board(win, C, R, CELL_SIZE)
+
+
+draw_menu_1(PADDING["left"], PADDING["top"], FONTS[1])
+
 
 while True:
     # 获取所有事件
@@ -92,23 +176,37 @@ while True:
             # Set the x, y postions of the mouse click
             ex, ey = event.pos
             if event.button == 1:
-                board.draw_wall_by_xy(ex, ey)
+                board.draw_wall_by_xy(ex - PADDING["left"], ey - PADDING["top"])
             elif event.button == 3:
-                board.remove_wall_by_xy(ex, ey)
+                board.remove_wall_by_xy(ex - PADDING["left"], ey - PADDING["top"])
 
         if event.type == pygame.MOUSEMOTION:
             if event.buttons[0] == 1:
                 ex, ey = event.pos
-                board.draw_wall_by_xy(ex, ey)
+                board.draw_wall_by_xy(ex - PADDING["left"], ey - PADDING["top"])
             elif event.buttons[2] == 1:
                 ex, ey = event.pos
-                board.remove_wall_by_xy(ex, ey)
+                board.remove_wall_by_xy(ex - PADDING["left"], ey - PADDING["top"])
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                mods = pygame.key.get_mods()
+                if mods & pygame.KMOD_CTRL:
+                    filename = input("Please input file name: ")
+                    save_to_txt(board, filename)
+            elif event.key == pygame.K_d:
+                mods = pygame.key.get_mods()
+                if mods & pygame.KMOD_CTRL:
+                    filename = input("Please input file name:")
+                    try:
+                        board.load_txt(filename)
+                    except Exception as e:
+                        print("Load failed:", e)
 
-    win.fill(COLORS["bg"])
-    board.draw(win)
-
-    draw_lines(C, R, win)
+    center_surface.fill(COLORS["board"])
+    board.draw(center_surface)
+    draw_lines(C, R, center_surface)
+    win.blit(center_surface, (PADDING["left"], PADDING["top"]))
 
     clock.tick(FPS) # 控制循环刷新频率,每秒刷新FPS对应的值的次数
     pygame.display.update()
